0afac23a9f214718c96da14dfabc14d4b03a8d9c
Julia Alexeeva <julia4406@gmail.com>
2025-04-18 22:17:16 +0300

    Add event models

--- src/event/models.py
+++ src/event/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    class Format(models.TextChoices):
+        ONLINE = "Online"
+        OFFLINE = "Offline"
+
+    title = models.CharField(max_length=63)
+    description = models.TextField(blank=True)
+    event_date = models.DateField()
+    start_of_event = models.TimeField(blank=True, null=True)
+    location = models.CharField(max_length=63, blank=True, null=True)
+    event_format = models.CharField(max_length=15, choices=Format)
+    organizer = models.CharField(max_length=63)
+
+    def __str__(self):
+        if self.start_of_event:
+            return f"{self.title} on {self.event_date} at {self.start_of_event}"
+        return f"{self.title} on {self.event_date}"
