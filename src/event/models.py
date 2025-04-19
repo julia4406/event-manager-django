fa3d6d03b83947c80d32cbc3ea0891039db80eb3
Julia Alexeeva <julia4406@gmail.com>
2025-04-19 21:28:17 +0300

    Add User to Event

--- src/event/models.py
+++ src/event/models.py
@@ -1,3 +1,4 @@
+from django.conf.global_settings import AUTH_USER_MODEL
 from django.db import models
 
 
@@ -13,8 +14,27 @@ class Event(models.Model):
     location = models.CharField(max_length=63, blank=True, null=True)
     event_format = models.CharField(max_length=15, choices=Format)
     organizer = models.CharField(max_length=63)
+    participants = models.ManyToManyField(
+        AUTH_USER_MODEL,
+        through="EventUser",
+        related_name="events"
+    )
 
     def __str__(self):
         if self.start_of_event:
             return f"{self.title} on {self.event_date} at {self.start_of_event}"
         return f"{self.title} on {self.event_date}"
+
+
+class EventUser(models.Model):
+    class RegistrationStatus(models.TextChoices):
+        REGISTERED = "Registered"
+        CANCELLED = "Cancelled"
+
+    event = models.ForeignKey("Event", on_delete=models.CASCADE)
+    participant = models.ForeignKey("User", on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=15,
+        choices=RegistrationStatus,
+        default=RegistrationStatus.REGISTERED
+    )
