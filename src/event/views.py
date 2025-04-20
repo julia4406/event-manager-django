66caaca157dfc08c43dd0b9b501b0c02a6856d77
Julia Alexeeva <julia4406@gmail.com>
2025-04-20 10:57:25 +0300

    Add basic authentication

--- src/event/views.py
+++ src/event/views.py
@@ -1,9 +1,17 @@
+from rest_framework import generics
+from rest_framework.authentication import (
+    SessionAuthentication,
+    BasicAuthentication
+)
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from event.models import Event
-from event.serializers import EventSerializer
+from event.serializers import EventSerializer, UserSerializer
 
 
 class EventViewSet(ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
