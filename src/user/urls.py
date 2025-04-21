562e36764387cb0bbb124f8d5a4005841a5bcd48
Julia Alexeeva <julia4406@gmail.com>
2025-04-21 11:41:15 +0300

    Add filters to UserListViewSet (for admin), Profile - add separate endpoints (ParticipatedEventsView, OrganizedEventsView)

--- src/user/urls.py
+++ src/user/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from user import views
+from user.views import ParticipatedEventsView, OrganizedEventsView
 
 router = routers.DefaultRouter()
 router.register("manage", views.UserListViewSet)
@@ -9,7 +10,13 @@ router.register("manage", views.UserListViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("signup/", views.CreateUserView.as_view(), name="signup"),
-    path("profile/", views.ProfileView.as_view(), name="profile")
+    path("profile/", views.ProfileView.as_view(), name="profile"),
+    path("profile/participated-events/",
+        ParticipatedEventsView.as_view(),
+        name="participated-events"),
+    path("profile/organized-events/",
+        OrganizedEventsView.as_view(),
+        name="organized-events")
 ]
 
 app_name = "user"
--- src/user/views.py
+++ src/user/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import get_user_model
-from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView, \
+    ListAPIView
 from rest_framework.authentication import (
     SessionAuthentication,
     BasicAuthentication
@@ -7,7 +10,22 @@ from rest_framework.authentication import (
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 
+from event.serializers import EventSerializer
 from user.serializers import UserSerializer
+
+
+class UserListViewSet(ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAdminUser]
+    filter_backends = [SearchFilter, DjangoFilterBackend]
+    search_fields = [
+        "username",
+        "email",
+        "first_name",
+        "last_name"
+    ]
 
 
 class CreateUserView(CreateAPIView):
@@ -18,15 +36,40 @@ class CreateUserView(CreateAPIView):
 class ProfileView(RetrieveUpdateAPIView):
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
-    filter_backends = []
 
     def get_object(self):
         return self.request.user
 
 
-class UserListViewSet(ModelViewSet):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-    authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAdminUser]
-    filter_backends = []
+class ParticipatedEventsView(ListAPIView):
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = [
+        "title",
+        "location",
+        "organizer__username",
+    ]
+    filterset_fields = ["event_format", "event_date", "organizer"]
+    ordering_fields = ["event_date", "title"]
+    ordering = ["event_date"]
+
+    def get_queryset(self):
+        return self.request.user.events.all()
+
+
+class OrganizedEventsView(ListAPIView):
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = [
+        "title",
+        "location",
+        "participants__username"
+    ]
+    filterset_fields = ["event_format", "event_date", "participants"]
+    ordering_fields = ["event_date", "title"]
+    ordering = ["event_date"]
+
+    def get_queryset(self):
+        return self.request.user.organized_events.all()
